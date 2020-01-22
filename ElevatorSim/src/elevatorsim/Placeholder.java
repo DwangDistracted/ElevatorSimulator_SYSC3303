cc70000aa22e5bcdb1c5b28b4a840b3721517871
David Wang <david.lwang.1998@gmail.com>
2020-01-22 13:20:36 -0500

    Created Empty Eclipse Project
    created an empty eclipse project for future code

---
+++ ElevatorSim/src/elevatorsim/Placeholder.java
@@ -0,0 +1,7 @@
+package elevatorsim;
+
+public class Placeholder {
+	public static void main(String[] args) {
+		System.out.println("Not Implemented");
+	}
+}
---
+++ ElevatorSim/src/elevatorsim/test/PlaceholderTest.java
@@ -0,0 +1,12 @@
+package elevatorsim.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlaceholderTest {
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+}
