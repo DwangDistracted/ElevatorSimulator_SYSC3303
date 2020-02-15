f5febf7ea75a47555bc9eb5deb201fa985781483
thomas <thomaselmoleung@gmail.com>
2020-02-15 18:04:08 -0500

    added floor test and fixed bgus

--- ElevatorSim/src/elevatorsim/common/requests/ElevatorArrivalRequest.java
+++ ElevatorSim/src/elevatorsim/common/requests/ElevatorArrivalRequest.java
@@ -5,6 +5,8 @@ import elevatorsim.constants.Direction;
 /**
  * A request sent when the arrival sensor notifies the system that an elevator is
  * arriving to a floor
+ * 
+ * @author Michael Patsula
  */
 public class ElevatorArrivalRequest extends Request<ElevatorArrivalRequest> {
 	private static final long serialVersionUID = 2893575143882472896L;
@@ -16,6 +18,7 @@ public class ElevatorArrivalRequest extends Request<ElevatorArrivalRequest> {
 	public ElevatorArrivalRequest(int elevatorId, int arrivalFloor, Direction elevatorDirection) {
 		this.elevatorId = elevatorId;
 		this.arrivalFloor = arrivalFloor;
+		this.elevatorDirection = elevatorDirection;
 	}
 	
 	public int getArrivalFloor() {
